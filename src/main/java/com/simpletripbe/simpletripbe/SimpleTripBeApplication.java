263c73aa5375edfd3a471b863956b184f7666916
안정후 <austinan@naver.com>
2023-03-28 15:31:24 +0900

    feat: init project

---
+++ src/main/java/com/simpletripbe/simpletripbe/SimpleTripBeApplication.java
@@ -0,0 +1,13 @@
+package com.simpletripbe.simpletripbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTripBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleTripBeApplication.class, args);
+    }
+
+}
