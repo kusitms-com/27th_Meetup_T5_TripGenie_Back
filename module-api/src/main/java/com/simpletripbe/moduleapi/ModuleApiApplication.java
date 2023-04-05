417d2a69b4c4001da47eee48479d4adf23096126
안정후 <austinan@naver.com>
2023-04-05 18:06:53 +0900

    feat: adding module dependencies

--- module-api/src/main/java/com/simpletripbe/moduleapi/ModuleApiApplication.java
+++ module-api/src/main/java/com/simpletripbe/moduleapi/ModuleApiApplication.java
@@ -2,8 +2,12 @@ package com.simpletripbe.moduleapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.simpletripbe.moduledomain")
+@EntityScan("com.simpletripbe.moduledomain.community.entity")
+@EnableJpaRepositories(basePackages = "com.simpletripbe.moduledomain.community.repository")
 public class ModuleApiApplication {
 
 	public static void main(String[] args) {
--- module-api/src/main/java/com/simpletripbe/moduleapi/applications/community/controller/CommunityController.java
+++ module-api/src/main/java/com/simpletripbe/moduleapi/applications/community/controller/CommunityController.java
@@ -7,17 +7,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping(value = "/community")
 public class CommunityController {
 
     private final CommunityService communityService;
 
-    @GetMapping
+    @GetMapping(value = "/selectAll")
     public ResponseEntity<BaseResponseBody<List<InfoDTO>>> selectAllList() {
 
         List<InfoDTO> list = communityService.selectAllList();
