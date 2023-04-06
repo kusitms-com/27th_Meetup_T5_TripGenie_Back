3d9cbfe82247728cbb893dccf13855a65460e667
안정후 <austinan@naver.com>
2023-04-06 11:18:06 +0900

    feat: adding google oauth

---
+++ module-api/src/main/java/com/simpletripbe/moduleapi/applications/login/controller/OauthController.java
@@ -0,0 +1,28 @@
+package com.simpletripbe.moduleapi.applications.login.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.simpletripbe.moduleapi.applications.login.service.OauthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value="/auth")
+public class OauthController {
+
+    private final OauthService oauthService;
+
+    @GetMapping(value="/{socialLoginType}")
+    public void socialLoginType(@PathVariable(name="socialLoginType") String socialLoginType) throws IOException {
+        oauthService.request(socialLoginType);
+    }
+
+    @GetMapping(value="/{socialLoginType}/callback")
+    public void callback(
+            @PathVariable(name="socialLoginType") String socialLoginType,
+            @RequestParam(name="code") String code) throws JsonProcessingException {
+        oauthService.oauthLogin(socialLoginType, code);
+    }
+}
---
+++ module-api/src/main/java/com/simpletripbe/moduleapi/applications/login/dto/GoogleUser.java
@@ -0,0 +1,18 @@
+package com.simpletripbe.moduleapi.applications.login.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class GoogleUser {
+    private String id;
+    private String email;
+    private Boolean verified_email;
+    private String name;
+    private String given_name;
+    private String picture;
+    private String locale;
+}
