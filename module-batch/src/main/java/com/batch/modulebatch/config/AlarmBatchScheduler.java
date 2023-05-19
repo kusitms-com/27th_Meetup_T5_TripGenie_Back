e13a705004d5a19b8df09da73eabbe0da0282ba6
안정후 <austinan@naver.com>
2023-05-19 09:48:54 +0900

    test: job 순차적 실행 가능한지 여부 테스트

--- module-batch/src/main/java/com/batch/modulebatch/config/AlarmBatchScheduler.java
+++ module-batch/src/main/java/com/batch/modulebatch/config/AlarmBatchScheduler.java
@@ -1,9 +1,12 @@
 package com.batch.modulebatch.config;
 
-import com.batch.modulebatch.controller.AutoAlarmJobLauncherController;
-import com.batch.modulebatch.controller.MyBagJobLauncherController;
+import com.batch.modulebatch.scheduler.AutoAlarmJobScheduler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -14,13 +17,32 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class AlarmBatchScheduler {
 
-    @Qualifier
+    @Qualifier("myBagJob")
+    private final Job myBagJob;
+
+    @Qualifier("autoAlarmJob")
     private final Job autoAlarmJob;
 
-    private final AutoAlarmJobLauncherController autoAlarmJobLauncherController;
+    private final JobLauncher jobLauncher;
 
     @Scheduled(cron = "0 0/1 * * * ?")
     public void runBatchJob() throws Exception {
-        autoAlarmJobLauncherController.startJob(autoAlarmJob);
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("jobName", "myBagJob" + System.currentTimeMillis())
+                .toJobParameters();
+
+        JobExecution execution1 = jobLauncher.run(myBagJob, jobParameters);
+        while (execution1.isRunning()) {
+            // 대기
+        }
+
+        jobParameters = new JobParametersBuilder()
+                .addString("jobName", "autoAlarmJob" + System.currentTimeMillis())
+                .toJobParameters();
+
+        JobExecution execution2 = jobLauncher.run(autoAlarmJob, jobParameters);
+        while (execution2.isRunning()) {
+            // 대기
+        }
     }
 }
